11e0dd8866bf5421bec1a01a2c6ec96d64241c60
gabriel kennedy <gabriel-kennedy@hotmail.com>
2019-05-28 20:26:27 -0300

    Foi criado um exemplo de como fazer uma requisição REST usando o RestSharp. Criei um exemplo de um GET, já que deve ser somente isso que vamos utilizar nas API's.

---
+++ Services_API/Financeiro/ApiService/ExemploApiService.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Services_API.Financeiro.ApiService
+{
+    public class ExemploApiService
+    {
+        protected Financeiro financeiro;
+
+        public ExemploApiService()
+        {
+            financeiro = new Financeiro();
+        }
+
+        // Trocar object pela classe de retorno
+        public object ObterExemplo()
+        {
+            // Recurso ofericido pela API
+            var resource = "/exemplo/exemplo";
+
+            // Parâmetros a serem adicionados no recurso
+            var p = new Parameter { Name = "nomeDoParametro", Value = "valorDoParametro" };
+
+            // Podemos adicionar quanntos parâmetros forem necessários nessa lista
+            List<Parameter> param = new List<Parameter>
+            {
+                p
+            };
+
+            // Uso do SDK para chamadas HTTP
+            var resposta = financeiro.Get(resource, param);
+
+            if (resposta.StatusCode.Equals(HttpStatusCode.OK))
+            {
+                // O segundo parâmetro deve ser criado usando os mesmos nomes que são retornados pelo JSON. Resposta.Content é o JSON que é retornado da API.
+                var objetoAnonimo = JsonConvert.DeserializeAnonymousType(resposta.Content, new
+                {
+                    id = new long(),
+                    nickName = "",
+                    registration_date = new DateTime(),
+                    first_name = "",
+                    last_name = "",
+                    gender = "",
+                    contry_Id = "",
+                    email = ""
+                });
+
+                var objetoDeRetorno = new object()
+                {
+                    // Preencher aqui o objeto de retorno com as informações deserializadas no objeto anônimo
+                    // Ex: objetoDeRetorno.atributo = objetoAnonimo.informacao;
+                };
+                return objetoDeRetorno;
+            }
+            else if (resposta.StatusCode.Equals(HttpStatusCode.Unauthorized))
+            {
+                if (resposta.Content.Contains("Blablabla"))
+                {
+                    return new object();
+                }
+            }
+
+            throw new Exception("Não foi possível fazer a chamada na API do Financeiro");
+        }
+    }
+}
---
+++ Services_API/Financeiro/Financeiro.cs
@@ -0,0 +1,128 @@
+﻿using RestSharp;
+using System.Collections.Generic;
+
+namespace Services_API.Financeiro
+{
+    public class Financeiro
+    {
+        private RestClient client = new RestClient(ApiUrl);
+
+        static private string apiUrl = "https://api.exemplo.com";
+        static public string ApiUrl
+        {
+            get
+            {
+                return apiUrl;
+            }
+            set
+            {
+                apiUrl = value;
+            }
+        }
+
+        public IRestResponse Get(string resource)
+        {
+            return Get(resource, new List<Parameter>());
+        }
+
+        public IRestResponse Get(string resource, List<Parameter> param)
+        {
+            var request = new RestRequest(resource, Method.GET);
+            List<string> names = new List<string>();
+            foreach (Parameter p in param)
+            {
+                names.Add(p.Name + "=" + p.Value + "");
+                p.Type = ParameterType.UrlSegment;
+                request.AddParameter(p);
+            }
+
+            request.Resource = resource + "?" + string.Join("&", names.ToArray());
+
+            request.AddHeader("Accept", "application/json");
+
+            var response = ExecuteRequest(request);
+
+
+            return response;
+        }
+
+        public IRestResponse Post(string resource, List<Parameter> param, object body)
+        {
+            var request = new RestRequest(resource, Method.POST);
+            List<string> names = new List<string>();
+            foreach (Parameter p in param)
+            {
+                names.Add(p.Name + "=" + p.Value + "");
+                p.Type = ParameterType.UrlSegment;
+                request.AddParameter(p);
+            }
+
+            request.Resource = resource + "?" + string.Join("&", names.ToArray());
+
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Content-Type", "application/json");
+            request.RequestFormat = DataFormat.Json;
+
+            request.AddJsonBody(body);
+
+            var response = ExecuteRequest(request);
+
+
+
+            return response;
+        }
+
+        public IRestResponse Put(string resource, List<Parameter> param, object body)
+        {
+            var request = new RestRequest(resource, Method.PUT);
+            List<string> names = new List<string>();
+            foreach (Parameter p in param)
+            {
+                names.Add(p.Name + "=" + p.Value + "");
+                p.Type = ParameterType.UrlSegment;
+                request.AddParameter(p);
+            }
+
+            request.Resource = resource + "?" + string.Join("&", names.ToArray());
+
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Content-Type", "application/json");
+            request.RequestFormat = DataFormat.Json;
+
+            request.AddJsonBody(body);
+
+            var response = ExecuteRequest(request);
+
+
+
+            return response;
+        }
+
+        public IRestResponse Delete(string resource, List<Parameter> param)
+        {
+            var request = new RestRequest(resource, Method.DELETE);
+            List<string> names = new List<string>();
+            foreach (Parameter p in param)
+            {
+                names.Add(p.Name + "=" + p.Value + "");
+                p.Type = ParameterType.UrlSegment;
+                request.AddParameter(p);
+            }
+
+            request.Resource = resource + "?" + string.Join("&", names.ToArray());
+
+            request.AddHeader("Accept", "application/json");
+
+            var response = ExecuteRequest(request);
+
+
+            return response;
+        }
+
+        private IRestResponse ExecuteRequest(RestRequest request)
+        {
+            client.UserAgent = "Financeiro";
+            return client.Execute(request);
+        }
+    }
+}
