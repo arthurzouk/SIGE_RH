9a3c2710510b8742e49d189b5560059f9f86dc2a
gabriel kennedy <gabriel-kennedy@hotmail.com>
2019-05-28 17:53:35 -0300

    Para organizar as consultas do banco, criei uma classe abstrata que será o repositório. Onde já temos o CRUD implementado por meio de métodos que usam tipos genéricos. Para criar a classe para consulta basta criar um novo repositório e herdar desse repositório base.

---
+++ RH_Banco/Repository/Repository.cs
@@ -0,0 +1,64 @@
+﻿using RH_Banco.Context;
+using RH_Dominio.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace RH_Banco.Repository
+{
+    public abstract class Repository<TEntity> where TEntity : Entity, new()
+    {
+        protected RecursosHumanosContext Db;
+        protected DbSet<TEntity> DbSet;
+
+        public Repository(RecursosHumanosContext context)
+        {
+            Db = context;
+        }
+
+        public virtual void Adicionar(TEntity obj)
+        {
+            DbSet.Add(obj);
+        }
+
+        public virtual void Atualizar(TEntity obj)
+        {
+            var entry = Db.Entry(obj);
+            DbSet.Attach(obj);
+            entry.State = EntityState.Modified;
+        }
+
+        public virtual void Remover(Guid id)
+        {
+            var entity = new TEntity { Id = id };
+            DbSet.Remove(entity);
+        }
+
+        public IEnumerable<TEntity> Buscar(Expression<Func<TEntity, bool>> predicate)
+        {
+            return DbSet.Where(predicate);
+        }
+
+        public virtual TEntity ObterPorId(Guid id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public virtual IEnumerable<TEntity> ObterTodos()
+        {
+            return DbSet.ToList();
+        }
+
+        public virtual IEnumerable<TEntity> ObterTodosPaginado(int s, int t)
+        {
+            return DbSet.Take(t).Skip(s);
+        }
+
+        public int SaveChanges()
+        {
+            return Db.SaveChanges();
+        }
+    }
+}
