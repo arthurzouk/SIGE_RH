234d8221dde9a0849088bcc49ce8239b4217e221
gabriel kennedy <gabriel-kennedy@hotmail.com>
2019-05-28 17:31:33 -0300

    Foi organizado os projetos em pastas para melhorar a visualização. Pacote do EntityFramework instalado no Data, Mvc e Api da Web para poder trabalhar com o objeto complexo do EF em outras camadas além da Data. O banco irei fazer usando a estratégia Code First, as configurações básicas já foram configuradas para tal.

---
+++ RH_Banco/Context/RecursosHumanosContext.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace RH_Banco.Context
+{
+    public class RecursosHumanosContext : DbContext
+    {
+        public RecursosHumanosContext() : base("RecursosHumanos")
+        {
+
+        }
+
+        //public DbSet<COLOCAR_CLASSE_DOMINIO_AQUI> EbayAuthTokens { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Configurações básicas
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties()
+                .Where(p => p.Name == "Id" + p.ReflectedType.Name)
+                .Configure(p => p.IsKey());
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(100));
+
+            // Adicionar configurações de tabelas
+            //modelBuilder.Configurations.Add(new COLOCAR_CLASSE_DE_CONFIG_AQUI());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ RH_Banco/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+namespace RH_Banco.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<RH_Banco.Context.RecursosHumanosContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(RH_Banco.Context.RecursosHumanosContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
